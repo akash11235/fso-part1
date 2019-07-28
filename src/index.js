beb3bb2accb26a8db037b7aad74b7ac32fea788f
Akash Pandey <akskipadhai@gmail.com>
2019-07-28 08:14:18 +0530

    Refactored by adding component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App'; the files were dummy files and were deleted. App object was coming from there.
 //ReactDOM.render(<App />, document.getElementById('root'));
+
+const Hello = () => {
+    return (
+    <div>
+        <p>Hello World</p>
+    </div>
+    )
+}
 const App = () => {
-    const now = new Date()
-    const a = 10
-    const b = 20
-    //braces {} within HTML snippet define JS expressions which are then evaluated and embedded 
-    //in the final HTML which will be produced.
     return(
     <div>
-        
-        <p>Hello World, it is {now.toString()}</p>
-        <p>{a} plus {b} is {a + b}</p>
+        <h1>Greetings</h1>
+        <Hello />
     </div>
     
     )
