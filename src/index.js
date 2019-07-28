0cefc36b816322f8ef263fa888f87a6ab6afd12e
Akash Pandey <akskipadhai@gmail.com>
2019-07-28 08:01:02 +0530

    JS Expressins added to make it dynamic

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App'; the files were dummy files and were deleted. App object was coming from there.
 //ReactDOM.render(<App />, document.getElementById('root'));
-const App = () => (
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    //braces {} within HTML snippet define JS expressions which are then evaluated and embedded 
+    //in the final HTML which will be produced.
+    return(
     <div>
-        <p>Hello World</p>
+        
+        <p>Hello World, it is {now.toString()}</p>
+        <p>{a} plus {b} is {a + b}</p>
     </div>
-)
+    
+    )
+}
 ReactDOM.render(<App/>, document.getElementById('root'));
