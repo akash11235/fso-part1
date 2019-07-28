a489006dfc0d66b1c0290704c61d708e96c47c69
Akash Pandey <akskipadhai@gmail.com>
2019-07-28 08:37:10 +0530

    multi parameter prop demo

--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ import './index.css';
 const Hello = (props) => {
     return (
     <div>
-        <p>Hello {props.name}</p>
+        <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
     )
 }
 const App = () => {
+    const name="Peter" 
+    const age=10
     return(
     <div>
         <h1>Greetings</h1>
-        <Hello name="George"/>
-        <Hello name="Daisy"/>
-        <Hello name="Robert"/>
+        <Hello name="Maya" age={26+10}/>
+        <Hello name={name} age={age}/>
     </div>
     )
 }
