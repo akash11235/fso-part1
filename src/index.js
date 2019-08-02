bceedde02326c4f2580770630e1a302c80613069
Akash Pandey <akskipadhai@gmail.com>
2019-08-02 08:46:24 +0530

    Component helper functions - added bornYear component func

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import './index.css';
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 const Hello = (props) => {
+    const bornYear = () => {
+        const yearNow = new Date().getFullYear()
+        return yearNow - props.age
+    }
     return (
     <div>
         <p>Hello {props.name}, you are {props.age} years old</p>
+        <p>So you were probably born in {bornYear()}</p>
     </div>
     )
 }
